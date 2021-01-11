a8b71907e378eddcfd539c124311831345dd3658
shuiumu0579 <shuimu0579@gmail.com>
2021-01-11 17:00:28 +0800

    change

--- cloudfunctions/add_item/index.js
+++ cloudfunctions/add_item/index.js
@@ -1,20 +1,20 @@
 const cloud = require('wx-server-sdk')
 
 cloud.init({
-  env: cloud.DYNAMIC_CURRENT_NEW,
-  throwOnNotFound: false
+    env: cloud.DYNAMIC_CURRENT_NEW,
+    throwOnNotFound: false
 })
 
 const db = cloud.database()
 
-exports.main = async (event, context) => {
-  const wxContext = cloud.getWXContext()
-  return db.collection('photos').add({
-    data:{
-      due: new Date(),
-      _openid: wxContext.OPENID,
-      type: event.type || '其他',
-      fileID: event.fileID
-    }
-  })
+exports.main = async(event, context) => {
+    const wxContext = cloud.getWXContext()
+    return db.collection('photos').add({ // db前面是不是需要加上一个await??  await db.collection('photos')
+        data: {
+            due: new Date(),
+            _openid: wxContext.OPENID,
+            type: event.type || '其他',
+            fileID: event.fileID
+        }
+    })
 }
